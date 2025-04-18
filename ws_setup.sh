251c88349b6d5ba5067e7d01ef91b972a7f2c62c
Braden Wiens <casual.endvrs@gmail.com>
2025-04-18 09:17:17 -0600

    updates ws_setup process

--- ws_setup.sh
+++ ws_setup.sh
@@ -1,5 +1,15 @@
+# General ros setup
 source /opt/ros/jazzy/setup.bash
-source ./ros2_ws/install/setup.bash
+/usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
+
+# -----
+
+# Local jobs
+cd ./ros2_ws
+colcon build
+
+# Local ws setup
+source ./install/setup.bash
 
 
 
